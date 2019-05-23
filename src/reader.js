1ab548a559155b1502a94ffb9321c15f12e97ed9
Roberto Antelo <roberto_antelo@hotmail.com>
2019-05-23 18:34:02 -0500

    Work on reader's aspectRatio

--- src/reader.js
+++ src/reader.js
@@ -1,12 +1,48 @@
+let SCALE = 1;
+const book = {
+  aspectRatio: {
+    width: 640,
+    height: 360,
+    orientation: "landscape"
+  },
+  pages: [
+    [
+      {
+        type: 'text',
+        content: 'Hoolaha!',
+        size: 32,
+        fill: 'white'
+      }
+    ]
+  ]
+}
+
+const fitter = ( src_width, src_height, dest_width, dest_height ) => {
+  SCALE = min(dest_width/src_width, dest_height/src_height);
+  const fit = {
+    width: src_width * SCALE,
+    height: src_height * SCALE
+  }
+  return fit;
+}
+
 function setup() {
-    createCanvas(640, 360);
-    background(20);
+  const { width: book_w, height: book_h } = book.aspectRatio;
+  const { width: w, height: h } = fitter(book_w, book_h, windowWidth, windowHeight);
+
+  console.log('hello world');
+
+  console.log({w, h});
+  createCanvas(w, h);
+  background(20);
+  console.log(windowWidth);
 }
 
 function draw() {
-    fill("white");
-    textSize(32);
-    const txt = "hello Page";
-    const title_w = textWidth(txt);
-    text(txt, width / 2 - title_w / 2, height / 2)
+
+  fill(book.pages[0][0].fill);
+  textSize(book.pages[0][0].size * SCALE);
+  const txt = book.pages[0][0].content;
+  const title_w = textWidth(txt);
+  text(txt, width / 2 - title_w / 2, height / 2)
 }
